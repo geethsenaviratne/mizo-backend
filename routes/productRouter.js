fc73404904c96c52d04e89a0110a7e9947fe8d50
Geeth Seneviratne <it23226128@my.sliit.lk>
2026-01-02 21:21:59 +0530

    Update

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -8,6 +8,7 @@ const productRouter = express.Router();
 productRouter.post("/", createProduct)
 productRouter.get("/", getProducts)
 // allow optional productId in URL so clients can also send productId in body as a fallback
-productRouter.delete("/:productId?", deleteProduct)
+productRouter.delete("/", deleteProduct)
+productRouter.delete("/:productId", deleteProduct)
 
 export default productRouter;
